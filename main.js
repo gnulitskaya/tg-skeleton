3288123ea0d6e22e4a00a0476ca8a1af30fc7db9
gnulitskaya <webgnule@mail.ru>
2024-08-12 21:57:19 +0300

    edit makefile

--- main.js
+++ main.js
@@ -121,10 +121,10 @@ process.once('SIGINT', () => bot.stop('SIGINT'))
 process.once('SIGTERM', () => bot.stop('SIGTERM'))
 
 // Start your Express server
-// const PORT = 8443;
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
+const PORT = 8443;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
 
 
 bot.launch();
