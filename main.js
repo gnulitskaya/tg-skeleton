dc4f8de012fce99b08001df454195707cd0a7cd6
gnulitskaya <webgnule@mail.ru>
2024-08-20 17:49:29 +0300

    update logic

--- main.js
+++ main.js
@@ -140,7 +140,7 @@ app.post('/webhook', async (req, res) => {
         const paymentId = eventData.object.id;
         // const amount = eventData.object.amount.value;
         // const currency = eventData.object.amount.currency;
-        const chatId = eventData.metadata?.chat_id;
+        const chatId = eventData.object.metadata.chat_id;
         console.log('chatId', chatId);
         // Send a message to your Telegram bot
         await bot.telegram.sendMessage(chatId, 
