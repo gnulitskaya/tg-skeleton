8b86e8eb7ce35e377dcadae83ee73819401046b1
gnulitskaya <webgnule@mail.ru>
2024-08-26 22:06:48 +0300

    ads postgresql

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -5,7 +5,7 @@ class UserController {
         // const newUser = await db.query('INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email]);
         console.log('newUser', req);
         const newUser = await db.query('INSERT INTO users (status, full_name, telegram_nick, amount, currency, order_id, comment, adress, payment_method, chat_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *', 
-            [req.body.status, req.body.full_name, req.body.telegram_nick, req.body.amount, req.body.currency, req.body.order_id, req.body.comment, req.body.adress, req.body.payment_method, req.body.chat_id]);
+            [req.status, req.full_name, req.telegram_nick, req.amount, req.currency, req.order_id, req.comment, req.adress, req.payment_method, req.chat_id]);
 
         res.status(201).json(newUser.rows[0]);
         // res.json(newUser.rows[0])
